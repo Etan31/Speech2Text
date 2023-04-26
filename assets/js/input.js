50d76541ecbd689fe4ed563a2ec8fee1f2c41c75
Etan31 <tumbagatristanehron31@gmail.com>
2023-04-26 10:43:08 +0800

    Fixed record btn

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,78 +1,15 @@
-/////////////////////////////////////////////////////////////////////////////////////STOP RECORDING
-const clickToConvertButton = document.getElementById("click_to_convert");
-var isRecording = false; // added variable to track recording state
-document.addEventListener("DOMContentLoaded", () => {
-  if (!isRecording) {
-    // start recording if not already recording
-    isRecording = true;
-    window.SpeechRecognition = window.webkitSpeechRecognition;
-
-    const recognition = new SpeechRecognition();
-    recognition.interimResults = true;
-
-    recognition.addEventListener("result", (e) => {
-      const transcript = Array.from(e.results)
-        .map((result) => result[0])
-        .map((result) => result.transcript)
-        .join("");
-
-      document.getElementById("convert_text").innerHTML = transcript;
-      console.log(transcript);
-    });
-
-    recognition.start();
-  } else {
-    // stop recording if already recording
-    isRecording = false;
-    recognition.stop();
-  }
-});
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const clickToConvertButton = document.getElementById("click_to_convert");
-//   var isRecording = false;
-//   var recognition;
-
-//   clickToConvertButton.addEventListener("click", function () {
-//     if (!isRecording) {
-//       isRecording = true;
-//       window.SpeechRecognition = window.webkitSpeechRecognition;
-
-//       recognition = new SpeechRecognition(); // Remove const here to assign value to global variable
-//       recognition.interimResults = false;
-
-//       recognition.addEventListener("result", (e) => {
-//         const transcript = Array.from(e.results)
-//           .map((result) => result[0])
-//           .map((result) => result.transcript)
-//           .join("");
-
-//         if (e.results[0].isFinal) {
-//           document.getElementById("convert_text").innerHTML = transcript;
-//           console.log(transcript);
-//         }
-//       });
-
-//       recognition.start();
-//     } else {
-//       isRecording = false;
-//       recognition.stop();
-//     }
-//   });
-// });
-
-// ///////////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////////////STOP RECORDING
 // const clickToConvertButton = document.getElementById("click_to_convert");
-// var isRecording = false;
-// var recognition;
-
-// clickToConvertButton.addEventListener("click", function () {
+// var isRecording = false; // added variable to track recording state
+// let recognition = null;
+// document.addEventListener("DOMContentLoaded", () => {
 //   if (!isRecording) {
+//     // start recording if not already recording
 //     isRecording = true;
 //     window.SpeechRecognition = window.webkitSpeechRecognition;
 
-//     recognition = new SpeechRecognition(); // Remove const here to assign value to global variable
-//     recognition.interimResults = false;
+//     const recognition = new SpeechRecognition();
+//     recognition.interimResults = true;
 
 //     recognition.addEventListener("result", (e) => {
 //       const transcript = Array.from(e.results)
@@ -80,43 +17,49 @@ document.addEventListener("DOMContentLoaded", () => {
 //         .map((result) => result.transcript)
 //         .join("");
 
-//       if (e.results[0].isFinal) {
-//         document.getElementById("convert_text").innerHTML = transcript;
-//         console.log(transcript);
-//       }
+//       document.getElementById("convert_text").innerHTML = transcript;
+//       console.log(transcript);
 //     });
 
 //     recognition.start();
 //   } else {
+//     // stop recording if already recording
 //     isRecording = false;
 //     recognition.stop();
 //   }
 // });
 
-// /*
+///FIXING THE RECORDING WHEN THE MAIN IS RELOAD
+let isRecording = false;
+let recognition = null;
 
-// FOR THE APPENDING DATA BACK TO THE JSON FILE
+const startRecording = () => {
+  isRecording = true;
+  recognition = new window.webkitSpeechRecognition();
+  recognition.interimResults = true;
+  recognition.addEventListener("result", (event) => {
+    const transcript = Array.from(event.results)
+      .map((result) => result[0])
+      .map((result) => result.transcript)
+      .join("");
+    document.getElementById("convert_text").innerHTML = transcript;
+    console.log(transcript);
+  });
+  recognition.start();
+};
 
-// */
+const stopRecording = () => {
+  isRecording = false;
+  recognition.stop();
+};
 
-// // const fs = require('fs');
-// // const path = require('path');
-
-// // const filePath = path.join(__dirname, '../../saved_JSON_file/data.json');
-
-// // const fs = require("fs");
-// // const path = require("path");
-
-// // const filePath = path.join(__dirname, "../../saved_JSON_file/data.json");
-
-// // fs.readFile(filePath, (err, data) => {
-// //   if (err) throw err;
-// //   const fileData = JSON.parse(data);
-// //   const newData = { name: "Jo   hn", age: 30 };
-// //   fileData.push(newData);
-
-//   fs.writeFile(filePath, JSON.stringify(fileData), (err) => {
-//     if (err) throw err;
-//     console.log("Data added to file");
-//   });
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  const clickToConvertButton = document.getElementById("click_to_convert");
+  clickToConvertButton.addEventListener("click", () => {
+    if (!isRecording) {
+      startRecording();
+    } else {
+      stopRecording();
+    }
+  });
+});
